5489c849ce4cc10bda2b72539734db8086545da1
unknown <alexvoy.voy@yandex.ru>
2021-03-22 00:18:50 +0300

    new

--- app.js
+++ app.js
@@ -27,22 +27,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Массив разешённых доменов
-const allowedCors = [
-  'https://domainname.students.nomoredomains.icu',
-  'http://domainname.students.nomoredomains.icu',
-  'http://localhost:3000',
-  'http://localhost:8080',
-];
-
 app.use((req, res, next) => {
-  const { origin } = req.headers;
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  }
   next();
 });
 
