669ccc5017e9d25a6441268681516f44582d75f2
unknown <alexvoy.voy@yandex.ru>
2021-03-21 23:20:23 +0300

    new

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://praktikum.tk');
+  res.header('Access-Control-Allow-Origin', 'https://domainname.students.nomoredomains.icu');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
