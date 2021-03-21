3439f7eb4f8f8c5e0261d0e4328e98e5167fa7fc
unknown <alexvoy.voy@yandex.ru>
2021-03-21 23:19:40 +0300

    new

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://praktikum.tk');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+});
+
 const userValidate = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
