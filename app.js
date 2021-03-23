08b01af542e1184fb7ab595b00a1871e95aa2293
unknown <alexvoy.voy@yandex.ru>
2021-03-23 13:49:27 +0300

    new

--- app.js
+++ app.js
@@ -24,17 +24,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-const options = {
-  origin: [
-    '*',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-};
+// const options = {
+//   origin: [
+//     '*',
+//   ],
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+// };
 
-app.use('*', cors(options));
+app.use('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
