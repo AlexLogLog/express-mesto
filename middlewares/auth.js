709cb4fdec5912bb1b87198ca7c731c920c21d02
unknown <alexvoy.voy@yandex.ru>
2021-03-24 15:21:20 +0300

    new

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!authorization && !authorization.startsWith('Bearer ')) {
     return next(new UnauthorizedError());
   }
 
