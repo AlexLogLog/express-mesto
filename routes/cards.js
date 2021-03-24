76571d09b6a67f4c50a7cd366a30c93c0e5f4a03
unknown <alexvoy.voy@yandex.ru>
2021-03-24 18:54:10 +0300

    new

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const cards = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { isURL } = require('validator');
 const {
   likeCard,
   dislikeCard,
@@ -10,14 +12,42 @@ const {
 
 cards.get('/cards', getCards);
 
-cards.post('/cards', newCard);
+cards.post('/cards',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(40),
+      link: Joi.string().required().custom((value, helpers) => {
+        if (!isURL(value)) return helpers.error('Невалидная ссылка');
+        return value;
+      }),
+    }),
+  }),
+  newCard);
 
 cards.get('/cards/:userId', getCardId);
 
-cards.put('/cards/:cardId/likes', likeCard);
+cards.put('/cards/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  likeCard);
 
-cards.delete('/cards/:cardId/likes', dislikeCard);
+cards.delete('/cards/:cardId/likes',
 
-cards.delete('/cards/:cardId', deleteCard);
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }), dislikeCard);
+
+cards.delete('/cards/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  deleteCard);
 
 module.exports = cards;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const users = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { isURL } = require('validator');
 const {
   getUsers,
   getUserMe,
@@ -11,8 +13,24 @@ users.get('/users', getUsers);
 
 users.get('/users/me', getUserMe);
 
-users.patch('/users/me', patchUserInfo);
+users.patch('/users/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(40),
+      about: Joi.string().required().min(2).max(200),
+    }),
+  }),
+  patchUserInfo);
 
-users.patch('/users/me/avatar', patchUserAvatar);
+users.patch('/users/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().required().custom((value, helpers) => {
+        if (!isURL(value)) return helpers.error('Не корректный email');
+        return value;
+      }),
+    }),
+  }),
+  patchUserAvatar);
 
 module.exports = users;
